868d805e05f14b950f9a9f04369450ca0ace5c4b
Preeti Goel <preeti.goel10@gmail.com>
2020-06-26 18:52:31 +0530

    COMMIT

--- src/test/java/com/epam/AppTest.java
+++ src/test/java/com/epam/AppTest.java
@@ -1,7 +1,9 @@
 package com.epam;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -9,12 +11,37 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
+    StringOperation stringOperation;
+
+    @Before
+    public void initiateClass(){
+        this.stringOperation=new StringOperation();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testCase1(){
+        String result=stringOperation.removeAFromString("ABCD");
+        assertEquals("BCD",result);
+    }
+    @Test
+    public void testCase2(){
+        String result=stringOperation.removeAFromString("AACD");
+        assertEquals("CD",result);
+    }
+    @Test
+    public void testCase3(){
+        String result=stringOperation.removeAFromString("BACD");
+        assertEquals("BCD",result);
+    }
+    @Test
+    public void testCase4(){
+        String result=stringOperation.removeAFromString("BBAA");
+        assertEquals("BBAA",result);
+    }
+    @Test
+    public void testCase5(){
+        String result=stringOperation.removeAFromString("AABAA");
+        assertEquals("BAA",result);
     }
 }
